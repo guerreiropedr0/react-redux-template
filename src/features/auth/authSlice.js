02ae894100b8ec46a71bc65764f9ffd3240fa6ad
Pedro Guerreiro <pmoreiraguerreiro@gmail.com>
2023-05-01 16:24:26 +0100

    Add login fetch

--- src/features/auth/authSlice.js
+++ src/features/auth/authSlice.js
@@ -19,6 +19,25 @@ export const signup = createAsyncThunk('auth/signup', async (userData) => {
   return await response.json();
 });
 
+export const login = createAsyncThunk('auth/login', async (userData) => {
+  const response = await fetch('http://localhost:3000/api/auth/login', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(userData),
+  });
+
+  if (!response.ok) {
+    const errorData = await response.text();
+    console.log(errorData);
+    throw new Error(errorData);
+  }
+
+  setToken(response.headers.get("Authorization"));
+  return await response.json();
+})
+
 const authSlice = createSlice({
   name: 'auth',
   initialState: {
@@ -38,6 +57,20 @@ const authSlice = createSlice({
         state.errors = null;
       })
       .addCase(signup.rejected, (state, action) => {
+        state.user = null;
+        state.loading = false;
+        state.errors = action.error.message.split(',');
+      })
+
+      .addCase(login.pending, (state) => {
+        state.loading = true;
+      })
+      .addCase(login.fulfilled, (state, action) => {
+        state.loading = false;
+        state.user = action.payload.user;
+        state.errors = null;
+      })
+      .addCase(login.rejected, (state, action) => {
         state.user = null;
         state.loading = false;
         state.errors = action.error.message.split(',');
